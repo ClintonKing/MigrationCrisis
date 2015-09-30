11d750a086c57284ecb014db3c505aeebd2f549a
ClintonKing <kingcj@live.unc.edu>
2015-09-30 11:12:04 -0400

    minor updates

--- Slideshow/js/slideshow.js
+++ Slideshow/js/slideshow.js
@@ -5,6 +5,34 @@ $(document).ready(function(){
   $("#nextButton").hide();
 });
 
+var currentIndex = 0,
+  items = $('.row .act'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.row .act').eq(currentIndex);
+  items.hide();
+  item.show();
+}
+
+$('#nextButton').click(function() {
+  // clearInterval(autoSlide);
+  $('#backButton').show();
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('#backButton').click(function() {
+  // clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
 
 
 $("#startHere").click(function(){
@@ -18,28 +46,11 @@ $("#introP").click(function(){
   $("#nextButton").delay(2000).fadeIn();
 });
 
-$("#act2P").click(function(){
-  $("#vid1").delay(600).fadeIn();
-})
-
-
-
-
-
-
-
-
-
 $("#nextButton").click(function(){
-  $("#act1").hide();
-  $("#act2").show();
-  $("#act2P").delay(1000).fadeIn();
-  $("#backButton").show();
+  $("#act2P").delay(600).fadeIn();
 });
 
-
-$("#backButton").click(function(){
-  $("#act2").hide();
-  $("#act1").show();
-  $("#nextButton").show();
+$("#act2P").click(function(){
+  $("#vid1").delay(600).fadeIn();
+  $("#act2P").delay(500).hide();
 });
