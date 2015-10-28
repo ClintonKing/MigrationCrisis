53e48bf77cc62a0db157d80c638a5733987c4dde
andyroberson <andyroberson2@gmail.com>
2015-10-28 00:49:28 -0400

    changed menu so starts under bold, added all content to overview

--- scroll.js
+++ scroll.js
@@ -1,7 +1,8 @@
 $(window).scroll(function() {
     var height = $(window).scrollTop();
+    var windowHeight = $(window).height();
 
-    if(height > 50) {
+    /*if(height > 50) {
       $(".menu").css({"background-color": "#eee", "border-bottom": "solid 1px #d8d8d8", "margin-top": "0", "color": "blue", "font-size": "22px"});
       $(".menu-links").css({"margin-top": "15px"});
       $(".logo-title").css({"margin-top": "15px"});
@@ -15,5 +16,13 @@ $(window).scroll(function() {
       $(".logo-title").css({"margin-top": "25px"});
       $("a:link, a:active").css({"color": "black"});
       $(".logo").css({"background": "url('img/watercolor-boat-logov3.png')", "background-size": "95px 95px", "height": "95px", "width": "95px", "opacity": ".4"})
+    }*/
+
+    if (height > windowHeight) {
+      $(".menu").css({"top": "0px", "position": "fixed"})
+    }
+
+    if (height < windowHeight) {
+      $(".menu").css({"top": "100%", "position": "absolute"});
     }
 });
